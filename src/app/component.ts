c874ea58f0d1147f8dbc2cea070a7ab294f5e3b8
Rene Copaga <rene.copaga@gmail.com>
2020-05-18 15:58:56 -0400

    Preparing

--- src/app/component.ts
+++ src/app/component.ts
@@ -18,8 +18,17 @@ export class ProductComponent {
         return this.model.getProducts()[position];
     }
 
-    getClassesByPosition(position: number): string {
-        let product = this.getProductByPosition(position);
-        return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    getProduct(key: number): Product {
+        return this.model.getProduct(key);
     }
+
+    getProducts(): Product[] {
+        return this.model.getProducts();
+    }
+
+    getProductCount(): number {
+        return this.getProducts().length;
+    }
+
+    targetName: string = "Kayak";
 }
