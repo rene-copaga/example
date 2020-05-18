bb46d9385a91874272ffeadaeca50d2c8073a52f
Rene Copaga <rene.copaga@gmail.com>
2020-05-18 12:17:43 -0400

    Updating the Data in the Application

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, ApplicationRef } from "@angular/core";
 import { Model } from "./repository.model";
+import { Product } from "./product.model";
 
 @Component({
     selector: "app",
@@ -8,17 +9,17 @@ import { Model } from "./repository.model";
 export class ProductComponent {
     model: Model = new Model();
 
-    getClasses(key: number): string {
-        let product = this.model.getProduct(key);
-        return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    constructor(ref: ApplicationRef) {
+        (<any>window).appRef = ref;
+        (<any>window).model = this.model;
     }
 
-    getStyles(key: number) {
-        let product = this.model.getProduct(key);
-        return {
-            fontSize: "30px",
-            "margin.px": 100,
-            color: product.price > 50 ? "red" : "green"
-        };
+    getProductByPosition(position: number): Product {
+        return this.model.getProducts()[position];
+    }
+
+    getClassesByPosition(position: number): string {
+        let product = this.getProductByPosition(position);
+        return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
     }
 }
