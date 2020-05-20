1825809cdaaf469631bf1d07d96272fedaa6ac45
Rene Copaga <rene.copaga@gmail.com>
2020-05-20 11:27:42 -0400

    Preparing

--- src/app/component.ts
+++ src/app/component.ts
@@ -9,41 +9,11 @@ import { Product } from "./product.model";
 export class ProductComponent {
     model: Model = new Model();
 
-    constructor(ref: ApplicationRef) {
-        (<any>window).appRef = ref;
-        (<any>window).model = this.model;
-    }
-
-    getProductByPosition(position: number): Product {
-        return this.model.getProducts()[position];
-    }
-
     getProduct(key: number): Product {
         return this.model.getProduct(key);
     }
 
     getProducts(): Product[] {
         return this.model.getProducts();
-    }
-
-    getProductCount(): number {
-        console.log("getProductCount invoked");
-        return this.getProducts().length;
-    }
-
-    targetName: string = "Kayak";
-
-    getKey(index: number, product: Product) {
-        return product.id;
-    }
-
-    counter: number = 1;
-
-    get nextProduct(): Product {
-        return this.model.getProducts().shift();
-    }
-
-    getProductPrice(index: number): number {
-        return Math.floor(this.getProduct(index).price);
     }
 }
