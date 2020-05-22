0de694afb84ba2da30474968dd6b284ea45064e8
Rene Copaga <rene.copaga@gmail.com>
2020-05-22 00:17:39 -0400

    Adding a Form to the Example Application

--- src/app/component.ts
+++ src/app/component.ts
@@ -17,9 +17,13 @@ export class ProductComponent {
         return this.model.getProducts();
     }
 
-    selectedProduct: string;
+    newProduct: Product = new Product();
 
-    getSelected(product: Product): boolean {
-        return product.name == this.selectedProduct;
+    get jsonProduct() {
+        return JSON.stringify(this.newProduct);
+    }
+
+    addProduct(p: Product) {
+        console.log("New Product: " + this.jsonProduct);
     }
 }
