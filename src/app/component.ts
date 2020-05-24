85f2d1fac41372c96512dc5f9adda091e187bd86
Rene Copaga <rene.copaga@gmail.com>
2020-05-24 01:06:11 -0400

    Preparing

--- src/app/component.ts
+++ src/app/component.ts
@@ -27,7 +27,7 @@ export class ProductComponent {
     }
 
     addProduct(p: Product) {
-        console.log("New Product: " + this.jsonProduct);
+        this.model.saveProduct(p);
     }
 
     getValidationMessages(state: any, thingName?: string) {
