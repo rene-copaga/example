d14c1e47562aeba109694f5105b4ad57c068fd99
Rene Copaga <rene.copaga@gmail.com>
2020-05-26 00:15:12 -0400

    Responding to Input Property Changes

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Attribute, Input } from "@angular/core";
+import { Directive, ElementRef, Attribute, Input, SimpleChange } from "@angular/core";
 
 @Directive({
     selector: "[pa-attr]",
@@ -10,8 +10,14 @@ export class PaAttrDirective {
     @Input("pa-attr")
     bgClass: string;
 
-    ngOnInit() {
-        this.element.nativeElement.classList.add(this.bgClass || "bg-success", 
-            "text-white");
+    ngOnChanges(changes: {[property: string]: SimpleChange }) {
+        let change = changes["bgClass"];
+        let classList = this.element.nativeElement.classList;
+        if (!change.isFirstChange && classList.contains(change.previousValue)) {
+            classList.remove(change.previousValue);
+        }
+        if (!classList.contains(change.currentValue)) {
+            classList.add(change.currentValue);
+        }
     }
 }
