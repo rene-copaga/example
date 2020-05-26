2578937c27ec33ed92027222544bb53289ec5c93
Rene Copaga <rene.copaga@gmail.com>
2020-05-26 01:05:13 -0400

    Creating Host Element Bindings

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Attribute, Input, SimpleChange, Output, EventEmitter } from "@angular/core";
+import { Directive, ElementRef, Attribute, Input, SimpleChange, Output, EventEmitter, HostBinding, HostListener } from "@angular/core";
 import { Product } from "./product.model";
 
 @Directive({
@@ -6,15 +6,8 @@ import { Product } from "./product.model";
 })
 export class PaAttrDirective {
 
-    constructor(private element: ElementRef) {
-        this.element.nativeElement.addEventListener("click", e => {
-            if (this.product != null) {
-                this.click.emit(this.product.category);
-            }
-        });
-    }
-
     @Input("pa-attr")
+    @HostBinding("class")
     bgClass: string;
 
     @Input("pa-product")
@@ -23,14 +16,10 @@ export class PaAttrDirective {
     @Output("pa-category")
     click = new EventEmitter<string>();
 
-    ngOnChanges(changes: {[property: string]: SimpleChange }) {
-        let change = changes["bgClass"];
-        let classList = this.element.nativeElement.classList;
-        if (!change.isFirstChange && classList.contains(change.previousValue)) {
-            classList.remove(change.previousValue);
-        }
-        if (!classList.contains(change.currentValue)) {
-            classList.add(change.currentValue);
+    @HostListener("click")
+    triggerCustomEvent() {
+        if (this.product != null) {
+            this.click.emit(this.product.category);
         }
     }
 }
