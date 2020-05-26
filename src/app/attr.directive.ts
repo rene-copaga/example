3cfbba91c4b8be8a6127646be18f09d8f419660b
Rene Copaga <rene.copaga@gmail.com>
2020-05-25 22:24:25 -0400

    Creating Data-Bound Input Properties

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,11 +1,17 @@
-import { Directive, ElementRef, Attribute } from "@angular/core";
+import { Directive, ElementRef, Attribute, Input } from "@angular/core";
 
 @Directive({
     selector: "[pa-attr]",
 })
 export class PaAttrDirective {
 
-    constructor(element: ElementRef, @Attribute("pa-attr") bgClass: string) {
-        element.nativeElement.classList.add(bgClass || "bg-success", "text-white");
+    constructor(private element: ElementRef) {}
+
+    @Input("pa-attr")
+    bgClass: string;
+
+    ngOnInit() {
+        this.element.nativeElement.classList.add(this.bgClass || "bg-success", 
+            "text-white");
     }
 }
