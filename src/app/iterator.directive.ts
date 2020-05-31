bdfa135c1ff90bfef6d2e83e0785cab1990de7e3
Rene Copaga <rene.copaga@gmail.com>
2020-05-31 01:23:13 -0400

    Keeping Track of Views

--- src/app/iterator.directive.ts
+++ src/app/iterator.directive.ts
@@ -1,10 +1,11 @@
-import { Directive, ViewContainerRef, TemplateRef, Input, DefaultIterableDiffer, IterableDiffers, ChangeDetectorRef } from "@angular/core";
+import { Directive, ViewContainerRef, TemplateRef, Input, DefaultIterableDiffer, IterableDiffers, ChangeDetectorRef, ViewRef } from "@angular/core";
 
 @Directive({
     selector: "[paForOf]"
 })
 export class PaIteratorDirective {
     private differ: DefaultIterableDiffer<any>;
+    private views: Map<any, PaIteratorContext> = new Map<any, PaIteratorContext>();
 
     constructor(private container: ViewContainerRef,
         private template: TemplateRef<Object>,
@@ -25,29 +26,46 @@ export class PaIteratorDirective {
         if (changes != null) {
             console.log("ngDoCheck called, changes detected");
             changes.forEachAddedItem(addition => {
-                this.container.createEmbeddedView(this.template,
-                    new PaIteratorContext(addition.item,
-                        addition.currentIndex, changes.length));
+                let context = new PaIteratorContext(addition.item,
+                    addition.currentIndex, changes.length);
+                context.view = this.container.createEmbeddedView(this.template,
+                    context);
+                this.views.set(addition.trackById, context);
             });
+            let removals = false;
+            changes.forEachRemovedItem(removal => {
+                removals = true;
+                let context = this.views.get(removal.trackById);
+                if (context != null) {
+                    this.container.remove(this.container.indexOf(context.view));
+                    this.views.delete(removal.trackById);
+                }
+            });
+            if (removals) {
+                let index = 0;
+                this.views.forEach(context => 
+                    context.setData(index++, this.views.size));
+            }
         }
     }
 }
 
 class PaIteratorContext {
+    index: number;
     odd: boolean; even: boolean;
     first: boolean; last: boolean;
+    view: ViewRef;
 
     constructor(public $implicit: any,
-            public index: number, total: number) {
-        
+            public position: number, total: number) {
+        this.setData(position, total);
+    }
+
+    setData(index: number, total: number) {
+        this.index = index;
         this.odd = index % 2 == 1;
         this.even = !this.odd;
         this.first = index == 0;
         this.last = index == total - 1;
-
-        // setInterval(() => {
-        //     this.odd = !this.odd; this.even = !this.even;
-        //     this.$implicit.price++;
-        // }, 2000);
     }
 }
