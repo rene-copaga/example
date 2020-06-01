5fb4cf35641271049722986393fa34b46bf2b309
Rene Copaga <rene.copaga@gmail.com>
2020-06-01 16:11:52 -0400

    Using Input Properties to Coordinate Between Components

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { Model } from "./repository.model";
+import { Product } from "./product.model";
 
 @Component({
     selector: "paProductTable",
@@ -6,4 +8,20 @@ import { Component } from "@angular/core";
 })
 export class ProductTableComponent {
 
+    @Input("model")
+    dataModel: Model;
+
+    getProduct(key: number): Product {
+        return this.dataModel.getProduct(key);
+    }
+
+    getProducts(): Product[] {
+        return this.dataModel.getProducts();
+    }
+
+    deleteProduct(key: number) {
+        this.dataModel.deleteProduct(key);
+    }
+
+    showTable: boolean = true;
 }
