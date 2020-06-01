e07ab988d6e255c4334ae9fd28956a5cb4c5b7ff
Rene Copaga <rene.copaga@gmail.com>
2020-06-01 15:15:26 -0400

    Defining External Templates

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -2,9 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "paProductTable",
-    template:`<div class='bg-info p-2'>
-                This is the table component
-              </div>`
+    templateUrl: "productTable.component.html"
 })
 export class ProductTableComponent {
 
