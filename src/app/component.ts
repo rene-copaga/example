1c93e67fa6e72baea86695061658757320e4ebb4
Rene Copaga <rene.copaga@gmail.com>
2020-06-02 01:11:10 -0400

    Completing the Component Restructure

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,8 +1,6 @@
-import { Component, ApplicationRef } from "@angular/core";
+import { Component } from "@angular/core";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
-import { NgForm } from "@angular/forms";
-import { ProductFormGroup } from "./form.model";
 
 @Component({
     selector: "app",
@@ -10,65 +8,8 @@ import { ProductFormGroup } from "./form.model";
 })
 export class ProductComponent {
     model: Model = new Model();
-    form: ProductFormGroup = new ProductFormGroup();
-
-    getProduct(key: number): Product {
-        return this.model.getProduct(key);
-    }
-
-    getProducts(): Product[] {
-        return this.model.getProducts();
-    }
-
-    newProduct: Product = new Product();
-
-    get jsonProduct() {
-        return JSON.stringify(this.newProduct);
-    }
 
     addProduct(p: Product) {
         this.model.saveProduct(p);
     }
-
-    deleteProduct(key: number) {
-        this.model.deleteProduct(key);
-    }
-
-    getValidationMessages(state: any, thingName?: string) {
-        let thing: string = state.path || thingName;
-        let messages: string[] = [];
-        if (state.errors) {
-            for (let errorName in state.errors) {
-                switch (errorName) {
-                    case "required":
-                        messages.push(`You must enter a ${thing}`);
-                        break;
-                    case "minlength":
-                        messages.push(`A ${thing} must be at least
-                            ${state.errors['minlength'].requiredLength}
-                            characters`);
-                        break;
-                    case "pattern":
-                        messages.push(`The ${thing} contains
-                            illegal characters`);
-                        break;
-                }
-            }
-        }
-        return messages;
-    }
-
-    formSubmitted: boolean = false;
-
-    submitForm(form: NgForm) {
-        this.formSubmitted = true;
-        if (form.valid) {
-            this.addProduct(this.newProduct);
-            this.newProduct = new Product();
-            form.reset();
-            this.formSubmitted = false;
-        }
-    }
-
-    showTable: boolean = true;
 }
