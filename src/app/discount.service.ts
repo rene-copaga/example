b19b49f089af3d5436a6cd139cbbe3d0fc638faa
Rene Copaga <rene.copaga@gmail.com>
2020-06-03 17:36:57 -0400

    Preparing the Service

--- src/app/discount.service.ts
+++ src/app/discount.service.ts
@@ -1,10 +1,12 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
 export class DiscountService {
     private discountValue: number = 10;
 
     public get discount() : number {
         return this.discountValue;
     }
-    
     
     public set discount(newValue : number) {
         this.discountValue = newValue || 0;
