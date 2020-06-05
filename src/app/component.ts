9e504d0bec02af01fd0bd449e725a1ebe0f58e29
Rene Copaga <rene.copaga@gmail.com>
2020-06-05 15:16:35 -0400

    Updating the Root Component and Template

--- src/app/component.ts
+++ src/app/component.ts
@@ -4,15 +4,14 @@ import { Product } from "./product.model";
 
 @Component({
     selector: "app",
-    templateUrl: "template.html",
-    //styles: ["/deep/ div { border: 2px black solid; font-style:italic }"]
+    templateUrl: "template.html"
 })
 export class ProductComponent {
     //model: Model = new Model();
 
-    constructor(private model: Model) { }
+    // constructor(private model: Model) { }
 
-    addProduct(p: Product) {
-        this.model.saveProduct(p);
-    }
+    // addProduct(p: Product) {
+    //     this.model.saveProduct(p);
+    // }
 }
