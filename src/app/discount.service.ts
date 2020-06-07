3f06f49972ce82b8515a9672c886a508379a54a5
Rene Copaga <rene.copaga@gmail.com>
2020-06-07 16:41:14 -0400

    Using Opaque Tokens

--- src/app/discount.service.ts
+++ src/app/discount.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, Inject } from "@angular/core";
-import { LogService } from "./log.service";
+import { LogService, LOG_SERVICE } from "./log.service";
 
 @Injectable()
 export class DiscountService {
     private discountValue: number = 10;
 
-    constructor(@Inject("logger") private logger: LogService) { }
+    constructor(@Inject(LOG_SERVICE) private logger: LogService) { }
 
     public get discount() : number {
         return this.discountValue;
